2060778cbf2783143f8c90bc6f5addd4c4237bf3
harsh.ku <harsh.ku@media.net>
2020-09-13 00:09:19 +0530

    corrects few error, adds stub code

--- src/core/parser/parser.py
+++ src/core/parser/parser.py
@@ -7,23 +7,41 @@
 # TODO ideally there should be a common parser class and then subclass for each language for now creating a common one and adding python parser in it
 
 """
-This class contain parser methods for different languages
+This class contain parser methods for different languages.
+TODO convert the class to parserFactoy 
 """
 class Parser():
 
     def getTODOPython(self,filename):
         """
         This function extract the to dos for a given file
+        This will return the todo in this format 
+        {"filepath":"path/to/file.py","todo":[(lineno,message)]}
         """
         # There are two ways todo can be in a file after # or inside """
+        # Above line will also be returned by parser #logic :P 
         todoList = list()
+        todoDict = dict()
         with open(filename) as f:
             lines = f.readlines()
-            for line in lines():
-                if line.strip()[0] == "#":
+            for i in range(0,len(lines)):
+                if "#" in lines[i]: # since comment can be after the code as well
+                    stripedLine = lines[i][lines[i].find("#"):].strip().lower()
                     # TODO will modify the logic to adapt variants of todo
-                    # using a temp varibale to make code more readable
-                    lineLower = line.lower()
-                    if "todo" in lineLower:
-                        todoList.append(lineLower[lineLower.find("todo")+4:].strip())
-        return todoList
+                    if "todo" in stripedLine:
+                        todoList.append( (i+1,stripedLine[stripedLine.find("todo")+4:].strip()) )
+        if len(todoList) > 0:
+            todoDict["filepath"] = filename
+            todoDict["todo"] = todoList
+            return todoList
+        else:
+            return None
+
+# Stub Code to test parser
+def parser_test():
+    filename = "./parser.py"
+    parser_obj = Parser()
+    print(parser_obj.getTODOPython(filename))
+
+parser_test()
+
